b04656cc66097608be331046e3c36cccf9e30643
Lam Vu <lamvu@dhcp-rhodes-1828.redrover.cornell.edu>
2017-09-13 11:24:55 -0400

    square sub blocks added

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 # to play with, you can put them in an appropriately-named Makefile.in.
 # For example, the default setup has a Makefile.in.icc and Makefile.in.gcc.
 
-PLATFORM=icc
+PLATFORM=mac
 
 include Makefile.in.$(PLATFORM)
 DRIVERS=$(addprefix matmul-,$(BUILDS))
@@ -70,7 +70,7 @@ timing-%.csv: matmul-%
 
 .PHONY: plot
 plot:
-	python plotter.py $(BUILDS)
+	python2.7 plotter.py $(BUILDS)
 
 # ---
 
--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,14 +1,62 @@
 const char* dgemm_desc = "My awesome dgemm.";
+#include <stdlib.h>
+
 
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
+    // universal variables
     int i, j, k;
+    
+    // find total number of sub-blocks(P-by-P)
+    int rmdr, N, P, MM;
+    P = 2;               // 2x2 sub matrix
+    rmdr = M  % P;       // # of pads
+    MM   = M  + rmdr;    // MM size of padded matrix
+    N    = MM / P;       // N # of subrows/subcolumns
+    
+    // pad matrices A and B
+    double* AA = (double*) malloc(MM*MM*sizeof(double));
+    double* BB = (double*) malloc(MM*MM*sizeof(double));
+    double* CC = (double*) malloc(MM*MM*sizeof(double));
+    k = 0;
+    for (i = 0; i < MM; i++) {
+        for (j = 1; j < MM; j++) {
+            k++;
+            if ( (i || j) <= M ) {
+                AA[k] = A[k];
+                BB[k] = B[k];
+                CC[k] = 0.0;
+            } else {
+                AA[k] = 0.0;
+                BB[k] = 0.0;
+                CC[k] = 0.0;
+            }
+        }
+    }
+    
+    // matrix multiply
+    int ii, jj;
+    for (ii = 0; ii < N; ++ii ) {
+        for (jj = 0; jj < N; ++jj ) {
+            // mini block multiplication here
+            for (i = 0; i < P; ++i) {
+                for (j = 0; j < P; ++j) {
+                    for (k = 0; k < P; ++k) {
+                            double cij = CC[jj+j*P+i+ii];
+                            for (k = 0; k < P; ++k) {
+                                cij += AA[k*P+i+ii] * BB[jj+j*P+k];
+                            CC[jj+j*P+i+ii] = cij;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // assign paded CC to unpaded C
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
-            double cij = C[j*M+i];
-            for (k = 0; k < M; ++k)
-                cij += A[k*M+i] * B[j*M+k];
-            C[j*M+i] = cij;
+            C[j*P+i] = CC[j*P+i];
         }
     }
 }
